361b9599bfff64ebd05018b64dcbacc1b869c242
Lincon Agrizzi <lincon_ac@hotmail.com>
2022-08-23 13:14:13 -0300

    Criando a classe Posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace xadrez_console
 {
@@ -6,7 +7,16 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: " +P);
+
+            Console.ReadLine();
+
+
+        
         }
     }
 }
---
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace tabuleiro
+{
+    internal class Posicao {
+
+        public int linha { get; set; }
+        public int coluna { get; set; }
+
+        public Posicao( int linha, int coluna)
+        {
+            this.linha = linha;
+            this.coluna = coluna;
+
+        }
+
+        public override string ToString()
+        {
+            return linha
+                + ", "
+                + coluna;
+        }
+    }
+}
